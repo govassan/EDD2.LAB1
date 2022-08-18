d4f96b55c1b1eb387cb055b8b985bb04712de47c
omar vasquez <govassan@gmail.com>
2022-08-18 13:14:38 -0500

    CARGA DE ARCHIVO LISTO

--- EDD2.LAB1/EDD2.LAB1/Controllers/HomeController.cs
+++ EDD2.LAB1/EDD2.LAB1/Controllers/HomeController.cs
@@ -7,10 +7,17 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Web;
+
 namespace EDD2.LAB1.Controllers
 {
     public class HomeController : Controller
+
     {
+        public static List<Persona> personas = new List<Persona>();//lista global desde archivo
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -22,16 +29,61 @@ namespace EDD2.LAB1.Controllers
         {
             return View();
         }
+
+        [Route("IngresarPersonas")]
         public IActionResult IngresarPersonas()
         {
             return View();
         }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost("IngresarPersonas")]
+        public IActionResult IngresarPersonas(IFormFile file)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (file == null)
+            {
+                try
+                {
+                    //subir archivo a una carpeta temporal
+                    string ruta = Path.Combine(Path.GetTempPath(), file.Name);
+                    using (var stream = new FileStream(ruta, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    //lectura de archivo
+                    string allFileData = System.IO.File.ReadAllText(ruta);
+                    //recorrer el archivo
+                    foreach (string lineaActual in allFileData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(lineaActual))
+                        {
+                            string[] informacion = lineaActual.Split(',');
+                            var personaActual = new Persona();
+
+
+                            //Guardar Informacion
+                            personaActual.tipo = informacion[0];
+                            personaActual.nombre = informacion[1];
+                            personaActual.dpi = informacion[2];
+                            personaActual.nacimiento = informacion[3];
+                            personaActual.direccion = informacion[4];
+                               
+
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+
+                    ViewBag.Error = e.Message;
+                }
+            }
+            return View(personas);
         }
+
+        public IActionResult MostrarPersonas()
+        {
+            return View();
+        }
+
     }
 }
--- EDD2.LAB1/EDD2.LAB1/Models/Persona.cs
+++ EDD2.LAB1/EDD2.LAB1/Models/Persona.cs
@@ -7,5 +7,25 @@ namespace EDD2.LAB1.Models
 {
     public class Persona
     {
+        public string tipo { get; set; }
+        public string nombre { get; set; }
+        public string dpi { get; set; }
+        public string nacimiento { get; set; }
+        public string direccion { get; set; }
+       
+
+        public Persona(string nombre, string dpi, string nacimiento, string direccion) //CONSTRUCTOR DE PERSONA
+        {
+            this.tipo = tipo;
+            this.nombre = nombre;
+            this.dpi = dpi;
+            this.nacimiento = nacimiento;
+            this.direccion = direccion;
+
+        }
+        public Persona()
+        {
+
+        }
     }
 }
